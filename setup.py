8e8b71bb96e6583401452bbf95a838de329eec00
flheight <75139846+flheight@users.noreply.github.com>
2024-07-12 22:19:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spectral-bridges",
-    version="1.0.0",
+    version="1.0.1",
     author="Félix Laplante",
     author_email="flheight0@gmail.com",
     description="Spectral Bridges clustering algorithm",
